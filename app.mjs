7225416a344c0f42b8fc8c059337a1e1f5007390
Will Simpson <will@kestrelcreek.com>
2024-12-04 20:12:24 -0800

    Update sidebar links and restructure sections in app configuration

--- app.mjs
+++ app.mjs
@@ -23,72 +23,18 @@ export default defineConfig({
   sidebar: {
     links: [
       {
-        url: "https://github.com/rothsandro/eleventy-notes",
-        label: "GitHub / Support",
+        url: "https://github.com/woodenzen",
+        label: "GitHub",
         icon: "github",
-      },
-      {
-        url: "https://www.buymeacoffee.com/sandroroth",
-        label: "Buy me a coffee",
-        icon: "coffee",
       },
     ],
     sections: [
       {
-        label: "Introduction",
+        label: "Notes",
         groups: [
           {
             query: createNotesQuery({
               pattern: "^/[^/]+$",
-            }),
-          },
-        ],
-      },
-      {
-        label: "Guides",
-        groups: [
-          {
-            label: "Writing Notes",
-            query: createNotesQuery({
-              pattern: "^/Writing/",
-              tree: {
-                replace: {
-                  "^/\\w+": "",
-                },
-              },
-            }),
-          },
-          {
-            label: "Organizing Notes",
-            query: createNotesQuery({
-              pattern: "^/Organizing/",
-            }),
-          },
-          {
-            label: "Core Features",
-            query: createNotesQuery({
-              pattern: "^/Features/",
-              tree: {
-                replace: {
-                  "^/\\w+": "",
-                },
-              },
-            }),
-          },
-          {
-            label: "Deployment",
-            query: createNotesQuery({
-              pattern: "^/Deployment/",
-            }),
-          },
-        ],
-      },
-      {
-        label: "Releases",
-        groups: [
-          {
-            query: createNotesQuery({
-              pattern: "^/Releases/",
             }),
           },
         ],
